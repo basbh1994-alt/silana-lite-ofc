19aedf15a10002eeea1a1482e5dee3ae892a7353
basbh1994-alt <basbh1994@gmail.com>
2025-10-04 21:25:10 +0300

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -52,9 +52,9 @@ global.media = {
 }
 // Social media
 global.url = {
- sig: 'https://instagram.com/noureddine_ouafy',
- sgh:  'https://instagram.com/noureddine_ouafy',
- sgc: 'https://whatsapp.com/channel/0029VaX4b6J7DAWqt3Hhu01A'
+ sig: 'https://instagram.com/lmm.l1',
+ sgh:  'https://instagram.com/lmm.l1',
+ sgc: 'https://whatsapp.com/channel/0029Vb3A0wrCnA7kStwYug0d'
 }
 // Donasi
 global.payment = {
