2f530c8e602bec3692f5d3152fd6b14e23a4c88c
basbh1994-alt <basbh1994@gmail.com>
2025-10-04 23:05:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -276,7 +276,7 @@ async function connectionUpdate(update) {
 ◦ *المنصة*: ${os.platform()} ${os.release()}
 ◦ *جهاز*: ${deviceName}
 ◦ *اسم البوت*: ${global.namebot}
-◦ *الوقت المتصل*: ${new Date().toLocaleString()}\n\n قناتي على الواتساب للمزيد من المعلومات \nhttps://whatsapp.com/channel/0029VaX4b6J7DAWqt3Hhu01A`;
+◦ *الوقت المتصل*: ${new Date().toLocaleString()}\n\n قناتي على الواتساب للمزيد من المعلومات \https://whatsapp.com/channel/0029Vb3A0wrCnA7kStwYug0d`;
         
         this.sendMessage(global.nomerown + `@s.whatsapp.net`, {
             text: message
@@ -319,7 +319,7 @@ global.reloadHandler = async function(restatConn) {
         conn.ev.off('connection.update', conn.connectionUpdate)
         conn.ev.off('creds.update', conn.credsUpdate)
     }
-    conn.welcome = 'مرحبا بك في أقوى مجموعة لبوتات الواتساب الرجاء قراءة قوانين المجموعة حتى لا يتم طردك  ، سيلانا اول بوت واتساب في الوطن العربي \n\n welcome to the groupe please read the rules of the group\n\n\n @subject, @user\n'
+    conn.welcome = 'مرحبا بك في أقوى مجموعة لبوتات الواتساب الرجاء قراءة قوانين المجموعة حتى لا يتم طردك  ، ذاك الامير سوف يقوم بطردك اذا خالفت  \n\n welcome to the groupe please read the rules of the group\n\n\n @subject, @user\n'
     conn.bye = '\n  مع السلامة  اتمنى ألا تعود الى هـــــــــنا \n@user 👋'
     conn.spromote = '@user *يرقي* إلى المشرف '
     conn.sdemote = '@user *خفض الرتبة* من المشرف'
